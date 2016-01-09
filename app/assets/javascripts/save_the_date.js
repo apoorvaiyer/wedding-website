06c90b13c49fdf44ec40b60d33dbe2f49ae93487
Apoorva Iyer <apoorva.iyer@gmail.com>
2016-01-09 14:43:19 -0800

    Wait for document ready before adding noise to the card

--- app/assets/javascripts/save_the_date.js
+++ app/assets/javascripts/save_the_date.js
@@ -1,10 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-alert('');
-$('.card-content').noisy({
-    'intensity' : 1, 
-    'size' : 200, 
-    'opacity' : 0.08, 
-    'fallback' : '', 
-    'monochrome' : false
-}).css('background-color', '#EDEBDE');
+$(function() {
+	$('.card-content').noisy({
+	    'intensity' : 1, 
+	    'size' : 200, 
+	    'opacity' : 0.08, 
+	    'fallback' : '', 
+	    'monochrome' : false
+	});
+});
